7cf155a4894664621503262ce8c955cc5860863c
Haim Marko <haim.marko@gmail.com>
2019-11-02 16:09:32 +0000

    take care of unexpected robocopy crush

--- windows/robocopy_wrapper.cmd
+++ windows/robocopy_wrapper.cmd
@@ -1,14 +1,25 @@
-
 c:\windows\system32\robocopy.exe %*
 @echo off 
-IF ERRORLEVEL 0 SET ERRORLEV=0
-IF ERRORLEVEL 1 SET ERRORLEV=0
-IF ERRORLEVEL 2 SET ERRORLEV=0
-IF ERRORLEVEL 4 SET ERRORLEV=0
-IF ERRORLEVEL 8 SET ERRORLEV=1
-IF ERRORLEVEL 16 SET ERRORLEV=1
 
-exit /B ERRORLEV=%ERRORLEV%
+echo KOKOKOK %ERRORLEVEL%
+
+IF %ERRORLEVEL% EQU 0 SET ERRORLEV=0
+IF %ERRORLEVEL% EQU 1 SET ERRORLEV=0
+IF %ERRORLEVEL% EQU 2 SET ERRORLEV=0
+IF %ERRORLEVEL% EQU 4 SET ERRORLEV=0
+IF %ERRORLEVEL% EQU 8 SET ERRORLEV=1
+IF %ERRORLEVEL% EQU 16 SET ERRORLEV=1
+
+echo YOYO %ERRORLEV%
+
+IF %ERRORLEVEL% EQU -1 (
+	SET ERRORLEV=1
+	echo ""
+	echo ""
+	echo ROBOCOPY TERMINATED UNEXPECTEDLY WiTH EXIT CODE %ERRORLEVEL%
+)
+
+exit /B %ERRORLEV%
 
 rem 0	No errors occurred and no files were copied.
 rem 1	One of more files were copied successfully.
@@ -16,3 +27,4 @@ rem 2	Extra files or directories were detected.  Examine the log file for more i
 rem 4	Mismatched files or directories were detected.  Examine the log file for more information.
 rem 8	Some files or directories could not be copied and the retry limit was exceeded.
 rem 16	Robocopy did not copy any files.  Check the command line parameters and verify that Robocopy has enough rights to write to the destination folder.
+rem -1  Robocopy been termintaed unexpectedly 
