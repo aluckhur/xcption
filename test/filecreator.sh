4584f6ce9df24296a7bd33a7056bb3ab0ac734f4
Haim Marko <haim.marko@gmail.com>
2019-11-02 07:53:12 +0000

    final

--- test/filecreator.sh
+++ test/filecreator.sh
@@ -6,14 +6,14 @@ do
 	do
 		for src in {1..3}
 		do
-			mkdir -p /xcp/src${src}/${dir}
-			dd if=/dev/sda of=/xcp/src${src}/${dir}/file${RANDOM} bs=${RANDOM} count=1
+			mkdir -p /mnt/cifssrc/dir1/src${src}/${dir}
+			dd if=/dev/sda of=/mnt/cifssrc/dir1/src${src}/${dir}/file${RANDOM} bs=100000 count=1
 		done	
 	done
 	RAND2=$(( ( RANDOM % 9 )  + 1 ))
 	for src in {1..3}
 	do
-		rm -rf /xcp/src${src}/dir${RAND2}*
+		rm -rf /mnt/cifssrc/dir1/src${src}/dir${RAND2}*
 	done
 	sleep 10
 done
