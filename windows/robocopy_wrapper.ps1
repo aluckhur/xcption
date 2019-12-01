42b50845fdb09c23729945e24577ce8313d19259
Haim Marko <haim.marko@gmail.com>
2019-12-01 12:35:49 +0000

    robocopy wrapper

--- windows/robocopy_wrapper.ps1
+++ windows/robocopy_wrapper.ps1
@@ -1,5 +1,3 @@
-
-
 $RobocopyErrorCodes = @{}
 $RobocopyErrorCodes[0] = "No errors occurred and no files were copied."
 $RobocopyErrorCodes[1] = "One of more files were copied successfully."
@@ -11,7 +9,6 @@ $RobocopyErrorCodes[-1] = "Robocopy been termintaed unexpectedly"
 
 $oInfo = New-Object System.Diagnostics.ProcessStartInfo
 $oInfo.FileName  = "robocopy"
-#$oInfo.Arguments = "/COPY:DATSO /MIR /DCOPY:DAT /R:0 /W:0 /V /BYTES /FP /LOG:c:\temp\rb.log /TEE \\192.168.0.200\c$\cifssrc\koko \\192.168.0.200\c$\cifsdst\koko"
 $oInfo.Arguments = $args
 $oInfo.UseShellExecute = $False
 $oInfo.RedirectStandardOutput = $True
@@ -30,6 +27,7 @@ $new = 0
 $filegone = 0
 $dirgone = 0
 $errors = 0
+$same = 0
 
 while (!$bDone)
 {
@@ -44,31 +42,41 @@ while (!$bDone)
     }
 
     $SpanTime = New-TimeSpan -Start $StartTime -End (Get-Date)
-
     $seconds = $SpanTime.Seconds
+    $minutes = $SpanTime.Minutes
+    $hours   = $SpanTime.Hours
+
     $logtick = [math]::truncate($seconds / 10)
 
     ForEach ($line in $lines) {
         if ($line) {
-        	if ($line -match '\s+Modified\s+(\d+)') {
+            if ($line -match '\s+Modified\s+(\d+)') {
                 $modified += 1
-        	} elseif ($line -match '\s+New File\s+(\d+)') {
+            } elseif ($line -match '\s+Newer\s+(\d+)') {
+                $modified += 1                                             
+            } elseif ($line -match '\s+New File\s+(\d+)') {
                 $new += 1  
-        	} elseif ($line -match '\s+New Dir\s+(\d+)') {
+            } elseif ($line -match '\s+New Dir\s+(\d+)') {
                 $new += 1 
-            } elseif ($line -match '\s+\*EXTRA File\s+(\d+)') {
+                                    } elseif ($line -match '(^|\s+)same\s+(\d+)') {
+                $same += 1               
+                                    } elseif ($line -match '\s+tweaked\s+(\d+)') {
+                $same += 1                                       
+            } elseif ($line -match '^\s*\*EXTRA File\s+(\d+)') {
                 $filegone += 1 
-            } elseif ($line -match '\s+\*EXTRA Dir\s+-?(\d+)') {
+            } elseif ($line -match '^\s*\*EXTRA Dir\s+-?(\d+)') {
                 $dirgone += 1 
-        	} elseif ($line -match '^\s+(\d+)\s+\\.+\\\s*$') {
+            } elseif ($line -match '^\s+(\d+)\s+\\.+\\\s*$') {
                 $dir += 1 
             } elseif ($line -match 'ERROR \d') {
                 Write-Host $line 
                 $errors += 1
+            } elseif ($line -match '^\s*\\\\') {
+                #skip split lines                                             
             } elseif ($line -match '^\s*(\d+)%\s*$') {
                 #skipp
             } elseif ($line -match '^\s*$') {
-                #empty 	
+                #empty        
             } else {
                 Write-Host "$($line)"
             }
@@ -77,15 +85,10 @@ while (!$bDone)
     
     if ($logtick -ne $oldlogtick -or $processexited) {
         $oldlogtick = $logtick
-        $hours = [math]::truncate($seconds / 3600)
-        $seconds -= [int]$hours*3600 
-
-        $minutes = [math]::truncate($seconds / 60)
-        $seconds -= [int]$minutes*60
 
         $time = [string]$hours+'h'+[string]$minutes+'m'+[string]$seconds+'s'      
 
-        $scanned = $modified + $new      
+        $scanned = $modified + $new + $same   
         Write-Host $('{0:N0}' -f ($scanned)) 'scanned' $('{0:N0}' -f ($new)) 'copied' $('{0:N0}' -f ($modified)) 'modification' $('{0:N0}' -f ($errors)) 'error' $('{0:N0}' -f ($filegone)) 'file.gone' $('{0:N0}' -f ($dirgone)) 'dir.gone' $time
         #Write-Host "errors: $errors modified: $modified new: $new extra file: $extrafile $time"
     }
@@ -93,13 +96,12 @@ while (!$bDone)
     if ($processexited)
     {
         $exitcode = $oProcess.ExitCode 
-        if ($exitcode -in 0,1,2,4) {
+        if ($exitcode -le 16 -and $exitcode -ge 0) {
             $exitmessage = $RobocopyErrorCodes[$exitcode]
+                                    Write-Host "Original Exit Code: $exitcode"
             $exitcode = 0
-        } else {
-        #if ($exitcode -gt 7 -or $exitcode -eq -1) {
+        } else {                 
             $exitmessage = $RobocopyErrorCodes[$exitcode]
-            $exitcode = 1
         }
 
         Write-Host ""
