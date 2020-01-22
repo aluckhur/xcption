5b1c97cedc29ad835d7314d74a42246b61da9f6b
Haim Marko <haim.marko@gmail.com>
2020-01-21 23:52:17 -0500

    bug fix

--- xcption.py
+++ xcption.py
@@ -2616,9 +2616,6 @@ def check_smartasses_job_status (jobname):
 				if file.startswith('stderrlog_'): results['stderrlog'] = os.path.join(jobcachedir,file)
 				if file.startswith('stdoutlog_'): results['stdoutlog'] = os.path.join(jobcachedir,file)	
 
-			if results['stderrlog'] == '':
-				results['status'] = 'failed'
-
 	return results
 
 #get a list of soruces that should be created 
@@ -2923,6 +2920,7 @@ def smartasses_fs_linux_status_createcsv(args,createcsv):
 
 				if results['status'] == 'completed' and (resultshardlink['status'] in ['not started','completed','not relevant']):
 					#parsing log to tree
+					pp.pprint results
 					dirtree = smartasses_parse_log_to_tree(src,results['stdoutlog'])
 					dirtree = createtasksfromtree(dirtree, dirtree.get_node(src))
 
