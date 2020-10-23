91e392dc8d16bbb5cf00054a812d20eb5ca8141a
Haim Marko <haim.marko@gmail.com>
2020-10-23 11:00:06 -0400

    remove not needed info

--- system/xcption_get_file.sh
+++ system/xcption_get_file.sh
@@ -1,14 +1,10 @@
 #!/bin/bash
-SCRIPT_DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" >/dev/null 2>&1 && pwd )"
-XCP_REPO_MOUNT_POINT=${SCRIPT_DIR}/xcp_repo
-SLEEP_BETWEEN_RUNS=10
-
 /usr/bin/wget $1 --output-document $2
 
 if [ $? -eq 0 ]
 then
-	echo "success file downloading from $1 to $2 on `hostname`"
+	echo "success file download from $1 to $2 on `hostname`"
 	exit 0
 fi
-echo "failed file downloading from $1 to $2 on `hostname`"
+echo "failed file download from $1 to $2 on `hostname`"
 exit 1
