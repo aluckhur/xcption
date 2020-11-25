7759c7ae9ad01d77d71937d506883b9906550862
Haim Marko <haim.marko@gmail.com>
2020-11-25 08:34:51 +0200

    remove jobs from windows when service is restatted

--- windows/nomad_service.cmd
+++ windows/nomad_service.cmd
@@ -1,3 +1,4 @@
 mkdir c:\NetApp\XCP\log
 del /Q c:\NetApp\XCP\log\NomadLog.txt
+rmdir /Q /S C:\NetApp\XCP\lib\alloc
 c:\NetApp\XCP\nomad.exe agent -config=c:\NetApp\XCP\client.hcl > c:\NetApp\XCP\log\NomadLog.txt
