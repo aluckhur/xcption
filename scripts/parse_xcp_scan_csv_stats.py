db348e1d161f212325fa5e0db8a99f84ffda98af
Haim Marko <haim.marko@gmail.com>
2024-04-25 11:05:28 -0400

    xcp scan

--- scripts/parse_xcp_scan_csv_stats.py
+++ scripts/parse_xcp_scan_csv_stats.py
@@ -17,14 +17,15 @@ def parse_xcp_scan(csvpath):
                 # if not linearr[0] in stats:
                 #     stats[linearr[0]] = {}
                 phase1_parse.append(linearr)
-    #key values that split to 2 lines 
-    dual_lines = ['Maximum Values','Average Values','Top Space Users','Top File Owners','Top File Extensions',
-                       'Number of files','Space used','Directory entries','Depth','Accessed','Modified','Changed','Path']
     #key values that are in single line
     single_lines = ['Total count','Directories','Regular files','Symbolic links','Special files','Hard links','Multilink files',
                     'Space Saved by Hard links (KB)','Sparse data','Dedupe estimate','Total space used']
-
-    #key vlaused for multiple key/value per line 
+    #key values that are in triple line
+    triple_lines = ['Accessed','Modified','Changed','Top File Extensions']
+    #key values that split to 2 lines 
+    dual_lines = ['Maximum Values','Average Values','Top Space Users','Top File Owners',
+                       'Number of files','Space used','Directory entries','Depth','Path']
+    #key values used for multiple key/value per line 
     total_lines = ['Total space for regular files','Total space for symlinks','Total space for directories']
 
     count1 = 0
@@ -53,6 +54,16 @@ def parse_xcp_scan(csvpath):
                     stats[linearr[0]][val] = phase1_parse[count1+1][count2]
                 count2+=1
             count1 += 1
+        elif linearr[0] in triple_lines:
+            stats[linearr[0]] = {}
+            count2 = 1
+            for val in linearr[1:]:
+                if not val in stats[linearr[0]]:
+                    stats[linearr[0]][val] = {} 
+                    stats[linearr[0]][val]['count'] = phase1_parse[count1+1][count2]
+                    stats[linearr[0]][val]['size'] = phase1_parse[count1+2][count2]
+                count2+=1
+            count1 += 2            
         elif linearr[0] in single_lines:
             stats[linearr[0]]=linearr[1]
             if not stats[linearr[0]].isnumeric():
